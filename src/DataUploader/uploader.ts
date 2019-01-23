b0e84f1feefe8984fcbb3b59b4b50ff712c6265c
aarb1 <abahendwar@gmail.com>
2019-01-23 13:16:36 -0500

    readFIle

--- src/DataUploader/uploader.ts
+++ src/DataUploader/uploader.ts
@@ -3,13 +3,14 @@ import * as xlsx from 'xlsx';
 class DataUploader {
 
     parse(file: string){
-        var workbook: xlsx.WorkBook = xlsx.read(file);
+        var workbook: xlsx.WorkBook = xlsx.readFile(file);
         var sheetNameList: string[] = workbook.SheetNames;
-        console.log(xlsx.utils.sheet_to_json(workbook.Sheets[sheetNameList[0]])[1]);
+        console.log('Sheets: ' + sheetNameList);
+        console.log(xlsx.utils.sheet_to_json(workbook.Sheets[sheetNameList[8]]));
     }
 
 }
 
 const du = new DataUploader();
 
-du.parse('src\\DataUploader\\a.xls');
+du.parse('C://Users//aayush//Desktop//data.xls');
