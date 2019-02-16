4926d7045c2bc62fcf2e582ca4a5a5f7498d2330
aarb1 <abahendwar@gmail.com>
2019-02-16 15:56:39 -0500

    Data by location end points finished

--- src/ApplicationDataAccess/dataByLocationAccess.ts
+++ src/ApplicationDataAccess/dataByLocationAccess.ts
@@ -8,30 +8,69 @@ export class DataByLocationAccess extends SqlDataAccess {
         super(config);
     }
 
-    getDataByLocation(state: string, lga: string, ward: string, facility: string){
+    getDataByLocation(state: string, lga: string, ward: string, facility: string) {
+        
+        if (state != null && lga == null && ward == null && facility == null) {
+            return this.getDataByState(state);
+        } 
+        else if (state != null && lga != null && ward == null && facility == null) {
+            return this.getDataByLga(state, lga);
+        } 
+        else if (state != null && lga != null && ward != null && facility == null) {
+            return this.getDataByWard(state, lga, ward);
+        } 
+        else if (state != null && lga != null && ward != null && facility != null) {
+            return this.getDataByFacility(state, lga, ward, facility);
+        } 
+        else {
+            return Promise.resolve(null);
+        }
+    }
+
+    getDataByState(state: string) {
         return SqlDataAccess.sqlPool.then(pool => {
             return pool.request()
-          //  .input()
-            .query(`Select * FROM Data WHERE FacilityId = (Select Id FROM FacilityView WHERE StateId = (Select Id FROM State WHERE "Name" = 'state5'));
-            Select * FROM "Sets";
+            .query("Select * FROM Data WHERE FacilityId IN (Select Id FROM FacilityView WHERE StateId = (Select Id FROM State WHERE \"Name\" = '" + state +"'));"+
+            `Select * FROM "Sets";
+            Select * FROM Groups;
+            Select * FROM Metrics;`
+            );            
+        });
+    }
+
+    getDataByLga(state: string, lga: string) {
+        return SqlDataAccess.sqlPool.then(pool => {
+            return pool.request()
+            .query(
+            "Select * FROM Data WHERE FacilityId IN (Select Id FROM FacilityView WHERE LGAId = (Select Id FROM LGA WHERE \"Name\" = '" + lga + "' AND StateId = (Select Id FROM State WHERE \"Name\" = '" + state + "')))"+
+            `Select * FROM "Sets";
+            Select * FROM Groups;
+            Select * FROM Metrics;`
+            );            
+        });
+    }
+
+    getDataByWard(state: string, lga: string, ward: string) {
+        return SqlDataAccess.sqlPool.then(pool => {
+            return pool.request()
+            .query(
+            "Select * FROM Data WHERE FacilityId IN (Select Id FROM FacilityView WHERE WardId = (Select Id FROM Ward WHERE \"Name\" = '" + ward + "') AND LGAId = (Select Id FROM LGA WHERE \"Name\" = '"+ lga +"' AND StateId = (Select Id FROM State WHERE \"Name\" = '"+state+"')))"+
+            `Select * FROM "Sets";
+            Select * FROM Groups;
+            Select * FROM Metrics;`
+            );            
+        });
+    }
+
+    getDataByFacility(state: string, lga: string, ward: string, facility: string) {
+        return SqlDataAccess.sqlPool.then(pool => {
+            return pool.request()
+            .query(
+            "Select * FROM Data WHERE FacilityId IN (Select Id FROM FacilityView WHERE FacilityId = (Select Id FROM Facility WHERE \"Name\" = '"+facility+"') AND WardId = (Select Id FROM Ward WHERE \"Name\" = '"+ward+"') AND LGAId = (Select Id FROM LGA WHERE \"Name\" = '"+lga+"' AND StateId = (Select Id FROM State WHERE \"Name\" = '"+state+"')))"+
+            `Select * FROM "Sets";
             Select * FROM Groups;
             Select * FROM Metrics;`
             );            
         });
     }
 }
-
-var t = new DataByLocationAccess(config.get('sqlConfig'));
-
-var h = t.getDataByLocation('a','a','a','a').then(result => {
-
-
-    var r = {
-        Data: result.recordsets[0],
-        Sets: result.recordsets[1],
-        Groups: result.recordsets[2],
-        Metrics: result.recordsets[3]
-    };
-    var z = JSON.stringify(r);
-    console.log(r);
-})
