de503974da9bda7d22805b6e4243c2975e813d28
aarb1 <abahendwar@gmail.com>
2019-02-15 20:05:23 -0500

    updates

--- src/ApplicationDataAccess/dataByLocationAccess.ts
+++ src/ApplicationDataAccess/dataByLocationAccess.ts
@@ -1,5 +1,6 @@
 import { SqlDataAccess } from '../DataAccess/sqlDataAccess';
 import * as mssql from 'mssql';
+import * as config from 'config';
 
 export class DataByLocationAccess extends SqlDataAccess {
 
@@ -10,8 +11,27 @@ export class DataByLocationAccess extends SqlDataAccess {
     getDataByLocation(state: string, lga: string, ward: string, facility: string){
         return SqlDataAccess.sqlPool.then(pool => {
             return pool.request()
-            .input('lga', mssql.NVarChar, name)
-            .query('SELECT Id FROM LGA WHERE Name = @lga;');
+          //  .input()
+            .query(`Select * FROM Data WHERE FacilityId = (Select Id FROM FacilityView WHERE StateId = (Select Id FROM State WHERE "Name" = 'state5'));
+            Select * FROM "Sets";
+            Select * FROM Groups;
+            Select * FROM Metrics;`
+            );            
         });
     }
 }
+
+var t = new DataByLocationAccess(config.get('sqlConfig'));
+
+var h = t.getDataByLocation('a','a','a','a').then(result => {
+
+
+    var r = {
+        Data: result.recordsets[0],
+        Sets: result.recordsets[1],
+        Groups: result.recordsets[2],
+        Metrics: result.recordsets[3]
+    };
+    var z = JSON.stringify(r);
+    console.log(r);
+})
