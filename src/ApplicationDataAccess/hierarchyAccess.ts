949e082ee72490491043ff15b8c3fe2a96dbcfb1
aarb1 <abahendwar@gmail.com>
2019-02-17 01:08:43 -0500

    Hierarchy routes changed to only Id

--- src/ApplicationDataAccess/hierarchyAccess.ts
+++ src/ApplicationDataAccess/hierarchyAccess.ts
@@ -13,9 +13,9 @@ export class HierarchyAccess extends SqlDataAccess {
             return pool.request()
             .query(
                 `SELECT "Name", Id FROM State;
-                SELECT Lga."Name", Lga.Id AS LgaId, "State"."Name" AS "State" FROM Lga INNER JOIN State ON Lga.StateId = "State".id;
-                SELECT Ward."Name", Ward.Id AS WardId, Lga."Name" AS "LGA" FROM Ward INNER JOIN LGA ON Ward.LgaId = Lga.id;
-                SELECT Facility."Name", Facility.Id AS FacilityId, Ward."Name" AS "Ward" FROM Facility INNER JOIN Ward ON Facility.WardId = Ward.id;`
+                SELECT Lga."Name", Lga.Id AS Id, "State"."Name" AS "State" FROM Lga INNER JOIN State ON Lga.StateId = "State".id;
+                SELECT Ward."Name", Ward.Id AS Id, Lga."Name" AS "LGA" FROM Ward INNER JOIN LGA ON Ward.LgaId = Lga.id;
+                SELECT Facility."Name", Facility.Id AS Id, Ward."Name" AS "Ward" FROM Facility INNER JOIN Ward ON Facility.WardId = Ward.id;`
             );            
         });
     }
@@ -24,9 +24,9 @@ export class HierarchyAccess extends SqlDataAccess {
         return SqlDataAccess.sqlPool.then(pool => {
             return pool.request()
             .query(
-                `SELECT GroupName, Id From Groups;
-                SELECT "Sets".SetName, "Sets".Id AS SetId, Groups.GroupName FROM "Sets" INNER JOIN Groups ON "Sets".GroupId = Groups.Id;
-                SELECT Metrics.MetricName, Metrics.Id AS MetricId, "Sets".SetName FROM Metrics INNER JOIN "Sets" ON Metrics.SetId = "Sets".Id;`
+                `SELECT GroupName, Id From Groups AS Id;
+                SELECT "Sets".SetName, "Sets".Id AS Id, Groups.GroupName FROM "Sets" INNER JOIN Groups ON "Sets".GroupId = Groups.Id;
+                SELECT Metrics.MetricName, Metrics.Id AS Id, "Sets".SetName FROM Metrics INNER JOIN "Sets" ON Metrics.SetId = "Sets".Id;`
             );            
         });
     }
