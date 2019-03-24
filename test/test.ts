14ba97d6edfc2f2174bda2a490deecf2d908ebb3
aarb1 <abahendwar@gmail.com>
2019-03-24 14:06:26 -0400

    Setup mocha chai

---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe('Hello function', () => {
+  it('should return hello world', () => {    
+        var tom = "tom";
+        expect(tom).to.equal('tom');
+  });
+});
