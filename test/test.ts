751e074e37b436b5665e01fb04738be077a669a1
Jackie Ngo <jngo42@uwo.ca>
2019-03-24 16:12:59 -0400

    completed dashboard unit tests

--- test/test.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-
-describe('Hello function', () => {
-  it('should return hello world', () => {    
-        var tom = "tom";
-        expect(tom).to.equal('tom');
-  });
-});
